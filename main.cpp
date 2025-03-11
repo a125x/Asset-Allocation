a2e75ecbcaed027d64796e23f07ad54e3bae06ff
user <alexander.andr.shat@gmail.com>
2025-03-11 17:28:18 +0300

    kind cheated on crypto - otherwise itd be hard to calibrate values, but for now Ive just assigned them value of 1 ruble and put amount in rubles as crypto amount. But yeah this shit needs calibration, will have to fix it later. Also added third output with diff values (also it might be helpful to add prompt to enter amount of money user wants to allocate), with an additional message about situation with crypto. and, well, updated portfolio with new spendings and a bit different structrue - finally decided to invest 10% in crypto, kinda risky but well, thats basically the only way for russians now to diversify portfolio outside of their country. kinda long commit message but whatever, work doesnt go well today and im pretty sick uaagh fuck it

--- main.cpp
+++ main.cpp
@@ -49,13 +49,16 @@ private:
 //("TICKER", percentage, price, amount of shares)
 vector<Fund> getPortfolio()
 {
-	Fund TGLD("GOLD",	10,	8.51,	500	);	// gold from tinkoff
-	Fund SBGB("SBGB",	10,	11.43,	300	);	// goverment bonds from sber
-	Fund SBRB("SBRB",	5,	13.29,	200	);	// corporate bonds from sber
-	Fund OBLG("OBLG",	5,	141.42,	20	);	// corporate bonds from vim (vtb)
-	Fund TMOS("TMOS",	40,	6.85,	6000	);	// russian stocks from tinkoff
-	Fund EQMX("TMOS",	30,	142.25,	69	);	// russian stocks from tinkoff
-	return { TGLD, SBGB, SBRB, OBLG, TMOS, EQMX };
+	Fund SBGD("SBGD",	15,	24.52,	6000	);	// gold from sber
+	Fund SBGB("SBGB",	7.5,	12.98,	5000	);	// goverment bonds from sber
+	Fund SBRB("SBRB",	2.5,	14.84,	3000	);	// corporate bonds from sber
+	Fund OBLG("OBLG",	2.5,	158.1,	300	);	// corporate bonds from vim (vtb)
+	Fund BOND("BOND",	2.5,	1265.,	6	);	// corporate bonds from dohod
+	Fund TMOS("TMOS",	45,	7.020,	54892	);	// russian stocks from tinkoff
+	Fund EQMX("EQMX",	15,	154.5,	1000	);	// russian stocks from vim
+	Fund  BTC("BTC",	5,	1,	0	);	// bitcoin - i can't currently make it work so let's put it as just rubles allocated with a price of 1 rub
+	Fund  XMR("XMR",	5,	1,	6482	);	// monero
+	return { SBGD, SBGB, SBRB, OBLG, BOND, TMOS, EQMX, BTC, XMR };
 }
 
 // Prints fund ticker, percents in the portfolio, amount of your money in the fund and amount of fund units
@@ -86,13 +89,25 @@ void printPortfolio(vector<Fund> real_portfolio)
 	// print values:
 	for (size_t i = 0; i < real_portfolio.size()+1; ++i) 
 	{
-	    for (size_t j = 0; j < table.size(); ++j) 
-	        cout << std::setw(15) << table[j][i];
+		for (size_t j = 0; j < table.size(); ++j) 
+			cout << setw(15) << table[j][i];
 
-	    cout << '\n';
+		cout << '\n';
 	}
 
 	cout << endl << money << " rubles allocated" << endl << endl;
+}
+
+// Prints diff between actual and desired portfolio
+void printDiff(vector<Fund> orig, vector<Fund> result)
+{
+	cout << setw(15) << "Ticker" << setw(15) << "Amount" << endl;
+
+	for (size_t i = 0; i < orig.size(); ++i)
+	{
+		cout << setw(15) << result[i].getTicker();
+		cout << setw(15) << result[i].getAmount() - orig[i].getAmount() << endl;
+	}
 }
 
 // Distributes the remaining after buying funds by the data money in the same proportions to plase them
@@ -159,10 +174,18 @@ int main()
 {
 	double money = 0;
 	cin >> money;
+
 	cout << endl << "Here's your current portfolio parameters: " << endl << endl;
-	printPortfolio(getPortfolio());
+	vector<Fund> orig = getPortfolio();
+	printPortfolio(orig);
+
 	cout << endl << "Here's how you can build your desired portfolio: " << endl << endl;
-	printPortfolio(resultAllocation(money, getPortfolio()));
+	vector<Fund> result = resultAllocation(money, getPortfolio());
+	printPortfolio(result);
+
+	cout << endl << "Diff (for crypto in rubles): " << endl << endl;
+	printDiff(orig, result);
+
 	return 0;
 }
 
